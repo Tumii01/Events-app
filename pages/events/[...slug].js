b520a594e5acfe84234a62a7d869e150591251e6
Tumii01 <tumisangnke@gmail.com>
2023-09-14 11:04:57 +0200

    Intial commits to sec-9

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,39 +1,54 @@
-import { Fragment, useEffect, useState } from "react";
-import { useRouter } from "next/router";
+import { Fragment, useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
 import useSWR from 'swr';
+import Head from 'next/head';
 
-
-import { getFilteredEvents } from "../../helpers/api-util";
-import EventList from "../../components/events/event-list";
-import ResultsTitle from "../../components/events/results-title";
-import Button from "../../components/ui/button";
-import ErrorAlert from "../../components/ui/error-alert";
+import { getFilteredEvents } from '../../helpers/api-util';
+import EventList from '../../components/events/event-list';
+import ResultsTitle from '../../components/events/results-title';
+import Button from '../../components/ui/button';
+import ErrorAlert from '../../components/ui/error-alert';
 
 function FilteredEventsPage(props) {
-  const [loadedevents, setLoadedEvents] = useState()
+  const [loadedEvents, setLoadedEvents] = useState();
   const router = useRouter();
 
   const filterData = router.query.slug;
 
-  const {data, error} =useSWR('https://next-js-project-3b10e-default-rtdb.firebaseio.com/events.json')
+  const { data, error } = useSWR(
+    'https://next-js-project-3b10e-default-rtdb.firebaseio.com/events.json',
+    (url) => fetch(url).then(res => res.json())
+  );
 
   useEffect(() => {
     if (data) {
       const events = [];
 
-for(const key in data) {
-    events.push({
-        id:key,
-        ...data[key]
-    });
-}
+      for (const key in data) {
+        events.push({
+          id: key,
+          ...data[key],
+        });
+      }
 
-setLoadedEvents(events)
+      setLoadedEvents(events);
     }
-  }, [data])
-  
-  if (!loadedevents) {
-    return <p className="center">Loading...</p>;
+  }, [data]);
+
+  let pageHeadData = (
+    <Head>
+      <title>Filtered Events</title>
+      <meta name='description' content={`A list of filtered events.`} />
+    </Head>
+  );
+
+  if (!loadedEvents) {
+    return (
+      <Fragment>
+        {pageHeadData}
+        <p className='center'>Loading...</p>
+      </Fragment>
+    );
   }
 
   const filteredYear = filterData[0];
@@ -42,96 +57,113 @@ setLoadedEvents(events)
   const numYear = +filteredYear;
   const numMonth = +filteredMonth;
 
+  pageHeadData = (
+    <Head>
+      <title>Filtered Events</title>
+      <meta
+        name='description'
+        content={`All events for ${numMonth}/${numYear}.`}
+      />
+    </Head>
+  );
+
   if (
     isNaN(numYear) ||
     isNaN(numMonth) ||
     numYear > 2030 ||
     numYear < 2021 ||
     numMonth < 1 ||
-    numMonth > 12 || error
+    numMonth > 12 ||
+    error
   ) {
     return (
       <Fragment>
+        {pageHeadData}
         <ErrorAlert>
           <p>Invalid filter. Please adjust your values!</p>
         </ErrorAlert>
-        <div className="center">
-          <Button link="/events">Show All Events</Button>
+        <div className='center'>
+          <Button link='/events'>Show All Events</Button>
         </div>
       </Fragment>
     );
   }
 
-
-
-
-  const filteredEvents = loadedevents.filter((event) => {
+  const filteredEvents = loadedEvents.filter((event) => {
     const eventDate = new Date(event.date);
-    return eventDate.getFullYear() === numYear && eventDate.getMonth() === numMonth - 1;
+    return (
+      eventDate.getFullYear() === numYear &&
+      eventDate.getMonth() === numMonth - 1
+    );
   });
 
   if (!filteredEvents || filteredEvents.length === 0) {
     return (
       <Fragment>
+        {pageHeadData}
         <ErrorAlert>
           <p>No events found for the chosen filter!</p>
         </ErrorAlert>
-        <div className="center">
-          <Button link="/events">Show all Events</Button>
+        <div className='center'>
+          <Button link='/events'>Show All Events</Button>
         </div>
       </Fragment>
     );
   }
+
   const date = new Date(numYear, numMonth - 1);
+
   return (
     <Fragment>
+      {pageHeadData}
       <ResultsTitle date={date} />
       <EventList items={filteredEvents} />
     </Fragment>
   );
 }
 
+// export async function getServerSideProps(context) {
+//   const { params } = context;
 
-// export async function getServersideProps(context) {
-// const {params} = context;
+//   const filterData = params.slug;
 
-// const filterData = params.slug;
+//   const filteredYear = filterData[0];
+//   const filteredMonth = filterData[1];
 
+//   const numYear = +filteredYear;
+//   const numMonth = +filteredMonth;
 
-// const filteredYear = filterData[0];
-// const filteredMonth = filterData[1];
-
-// const numYear = +filteredYear;
-// const numMonth = +filteredMonth;
-
-// if (
-//   isNaN(numYear) ||
-//   isNaN(numMonth) ||
-//   numYear > 2030 ||
-//   numYear < 2021 ||
-//   numMonth < 1 ||
-//   numMonth > 12
-// ) {
-//   return {
-//     props: { hasError: true},
-   
+//   if (
+//     isNaN(numYear) ||
+//     isNaN(numMonth) ||
+//     numYear > 2030 ||
+//     numYear < 2021 ||
+//     numMonth < 1 ||
+//     numMonth > 12
+//   ) {
+//     return {
+//       props: { hasError: true },
+//       // notFound: true,
+//       // redirect: {
+//       //   destination: '/error'
+//       // }
+//     };
 //   }
-// }
 
-// const filteredEvents = await getFilteredEvents({
-//   year: numYear,
-//   month: numMonth,
-// });
+//   const filteredEvents = await getFilteredEvents({
+//     year: numYear,
+//     month: numMonth,
+//   });
 
 //   return {
-//   props: {
-//     events: filteredEvents,
-//     date: {
-//       year: numYear,
-//       month: numMonth
-//     }
-//   }
-// };
+//     props: {
+//       events: filteredEvents,
+//       date: {
+//         year: numYear,
+//         month: numMonth,
+//       },
+//     },
+//   };
 // }
 
 export default FilteredEventsPage;
--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,11 +1,12 @@
-import { Fragment } from "react";
+import { Fragment } from 'react';
+import Head from 'next/head';
 
-
-import EventSummary from "../../components/event-detail/event-summary";
-import EventLogistics from "../../components/event-detail/event-logistics";
-import EventContent from "../../components/event-detail/event-content";
-import {getEventById, getFeaturedEvents} from '../../helpers/api-util';
-import ErrorAlert from "../../components/ui/error-alert";
+import { getEventById, getFeaturedEvents } from '../../helpers/api-util';
+import EventSummary from '../../components/event-detail/event-summary';
+import EventLogistics from '../../components/event-detail/event-logistics';
+import EventContent from '../../components/event-detail/event-content';
+import ErrorAlert from '../../components/ui/error-alert';
+import Comments from '../../components/input/comments';
 
 function EventDetailPage(props) {
   const event = props.selectedEvent;
@@ -17,8 +18,16 @@ function EventDetailPage(props) {
       </div>
     );
   }
+
   return (
     <Fragment>
+      <Head>
+        <title>{event.title}</title>
+        <meta
+          name='description'
+          content={event.description}
+        />
+      </Head>
       <EventSummary title={event.title} />
       <EventLogistics
         date={event.date}
@@ -29,34 +38,33 @@ function EventDetailPage(props) {
       <EventContent>
         <p>{event.description}</p>
       </EventContent>
+      <Comments eventId={event.id} />
     </Fragment>
   );
 }
 
-
 export async function getStaticProps(context) {
   const eventId = context.params.eventId;
-const event = await getEventById(eventId)
 
-return {
-  props: {
-    selectedEvent: event
-  },
-  revalidate: 30
- };
+  const event = await getEventById(eventId);
+
+  return {
+    props: {
+      selectedEvent: event
+    },
+    revalidate: 30
+  };
 }
 
-
-
-export async function getStaticPaths (){
+export async function getStaticPaths() {
   const events = await getFeaturedEvents();
-  
-  const paths = events.map(event => ({params: { eventId: event.id}}));
+
+  const paths = events.map(event => ({ params: { eventId: event.id } }));
 
   return {
     paths: paths,
     fallback: 'blocking'
-  }
+  };
 }
 
 export default EventDetailPage;
