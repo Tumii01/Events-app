0aaf3d0a9843186cf68b5c7eae488e2d3309acb6
Tumii01 <tumisangnke@gmail.com>
2023-09-18 10:47:26 +0200

    Final Changes

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,21 +1,31 @@
-import { MongoClient } from "mongodb"; 
+
+import {connectDatabase, insertDocument} from '../../helpers/db-util'
 
 
 async function handler(req, res) {
-  if (req.method === 'POST') {
+  if (req.method === "POST") {
     const userEmail = req.body.email;
 
-    if (!userEmail || !userEmail.includes('@')) {
+    if (!userEmail || !userEmail.includes("@")) {
       res.status(422).json({ message: "Invalid email address" });
       return;
     }
-const client = await MongoClient.connect('mongodb+srv://Tumisang:biggesmall1998@cluster0.zq6mjge.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp'
-)
-const db =client.db('events');
 
-await db.collection ('newsletter').insertOne({email: userEmail});
-
-   client.close(); 
+    let client;
+    try {
+        client = await connectDatabase();
+    } catch (error){
+        res.status(500).json({message: 'Connecting to the database failed!'});
+    return;
+    }
+    
+    try {
+        await insertDocument(client, 'newsletter', {email: userEmail});
+        client.close();
+    } catch (error) {
+        res.status(500).json({message: 'Inserting data failed!'});
+        return;
+    }
 
     res.status(201).json({ message: "Signed Up!" });
   }
